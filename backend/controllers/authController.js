4d3c4ac75c0ec554da3616ba9e340b891248e618
Hurmaan <contactme.hurmaan@gmail.com>
2023-12-18 17:30:18 +0530

    feat: authenticate/onboard B-users

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,21 @@
-function addUserSubscriptions(req, res) {
+const Subscriber = require('../models/subscriber');
+
+async function addUserSubscriptions(req, res) {
   try {
+    const { email, urls } = req.body;
+
+    const isExistingSubscriber = await Subscriber.findOne({ email });
+
+    if (isExistingSubscriber) {
+      res.status(400).send("User already exists");
+      return;
+    }
+
+    const subscriber = new Subscriber({ email, urls });
+
+    await subscriber.save();
+
+    res.status(200).send("User created");
 
   } catch (err) {
     console.log(err);
